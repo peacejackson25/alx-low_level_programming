614a6d12862472a94b89ac7bfb58c81aaf5939ad
peacejackson25 <beckyjackson553@gmail.com>
2022-10-17 19:43:54 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
